6aee35e5c7809c4656283ceeac3fcf726179ee1e
William da Silva Vianna <william.vianna@gsuite.iff.edu.br>
2026-06-27 19:21:27 -0300

    comentários doxygen

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+"""@brief Ponto de entrada da aplicação PIFF-0054-ng.
+
+Executa o painel técnico Streamlit para visualização de ensaios
+industriais, anotação de observações e exportação de relatórios.
+"""
+
 from src.ui.streamlit_app import main
 
 
